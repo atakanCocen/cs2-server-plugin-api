3d27f444c9f24d572043b2498817eb9b495474cb
atakanCocen <atakan1998@gmail.com>
2024-06-04 19:59:28 -0600

    initial setup

---
+++ src/main/java/com/hadleycocen/cs2serverpluginapi/Cs2serverpluginapiApplication.java
@@ -0,0 +1,13 @@
+package com.hadleycocen.cs2serverpluginapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Cs2serverpluginapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Cs2serverpluginapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hadleycocen/cs2serverpluginapi/Cs2serverpluginapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hadleycocen.cs2serverpluginapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Cs2serverpluginapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
